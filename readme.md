eee7e79f76b7aaf7a027eef647e6a194336d87c6
Sajjad Rad <sajjad.273@gmail.com>
2013-01-31 19:59:57 +0330

    * Sub menu supported. * Last item showing fixed.

--- readme.md
+++ readme.md
@@ -1,29 +1,31 @@
-=== Plugin Name ===
-Contributors: sajjadrad
-Tags: bootstrap, wordpress navmenu
-Requires at least: 3.0.1
-Tested up to: 3.4
-License: GPLv2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
+WP Bootstrap Navmenu
+====
 
-Convert Wordpress nav menu to Twitter Bootstrap style.
+* Contributors: sajjadrad
+* Tags: bootstrap, wordpress navmenu
+* Requires at least: 3.0.1
+* Tested up to: 3.4
+* License: GPLv2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-== Description ==
+
+Description
+===
 
 WP Bootstrap Menu convert wordpress nav menu style to Twitter Bootstrap [http://twitter.github.com/bootstrap/] navmenu style.
-You can join to WP Bootstrap Menu developing [https://github.com/sajjadrad/wp-bootstrap-navmenu] .
+You can join to WP Bootstrap Menu developing [https://github.com/sajjadrad/wp-bootstrap-navmenu].
 Compatible with Bootstrap 2.2.2 and support sub menu.
 
-
-== Installation ==
+Installation
+===
 
 1. Upload `wp-bootstrap-navmenu.php` to the `/wp-content/plugins/wp-bootstrap-navmenu/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
 1. Style your bootstrap navmenu and place `<?php echo getNavMenu( MENU NAME ); ?>` in your templates to get menu list.
 
 Example:
-<code>
-	<div class="navbar">
+```
+  <div class="navbar">
 		<div class="navbar-inner">  
  			<div class="container">
 				<ul class="nav">
@@ -34,25 +36,18 @@ Example:
 			</div>
 		</div>
 	</div>
-</code>
+```
 
 
-== Screenshots ==
-
-1. **Wordpress Navmenu**
-2. **Output Bootstrap style**
-3. **Submenu**
-
-== Changelog ==
-
-= 0.4.7 =
+Changelog
+===
+0.4.7
 * First release.
 
-= 0.4.8 =
+0.4.8
 * Fix Page title loading in link tag.
 * Fix showing last parent item.
 
-= 1.0.4 =
+1.0.4
 * Sub menu supported.
 * Last item showing fixed.
-
--- wp-bootstrap-navmenu.php
+++ wp-bootstrap-navmenu.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Plugin Name: WP Bootstrap Menu
-  Version: 0.4.8
+  Version: 1.0.4
   Description: Convert Wordpress nav menu to Twitter Bootstrap style.
   Plugin URI: http://wordpress.org/extend/plugins/wp-bootstrap-navmenu/
   Author: Sajjad Rad
@@ -13,49 +13,94 @@
     {
         $menu = $menuName; //Nav menu name
         $items = wp_get_nav_menu_items($menu); // Get nav menu items list
-        $isParent = False; //Checking this sub item is child of previous item or not
-        $firstChild = False; //Checking this sub item is first child of previous item or not
-        $hasChild = False; //Checking previous main item has a child or not
+        $level = 0;
         $last_title = "";
         $last_url = "";
+        $objectID_stack = array();
+        $objectIDStackTop = 0;
         $output = "";
         foreach ($items as $list)
         {
-
-            if ($list->menu_item_parent != "0")
+            if($list->menu_item_parent == "0")
             {
-
-                if ($isParent and $firstChild)
+                while(count($objectID_stack))
                 {
-                    $isParent = False;
-                    $firstChild = False;
-                    $hasChild = True;
-                    $output = $output."<li class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">".$last_title."<b class=\"caret\"></b></a><ul class=\"dropdown-menu\"><li><a href=\"".$list->url."\">".$list->title."</a></li>";
+                    array_pop($objectID_stack);
                 }
-                else if ($hasChild and !$firstChild)
-                {
-                    $isParent = False;
-                    $output = $output."<li><a href=\"".$list->url."\">".$list->title."</a></li>";
-                }
-            }
-            else
-            {
-                if ($isParent)
+                if($level == 1)
                 {
                     $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li>";
                 }
-                if ($hasChild)
+                if($level == 3)
                 {
-                    $output = $output."</ul></li>";
-                    $hasChild = False;
+                    $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li></ul></li></ul></li>";
                 }
-                $isParent = True;
-                $firstChild = True;
+                if($level == 2)
+                {
+                    $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li></ul></li>";
+                }
+                $level = 1 ;
+                array_push($objectID_stack, $list->object_id);
                 $last_title = $list->title;
                 $last_url = $list->url;
             }
+            else
+            {
+                $stackTop = count($objectID_stack)-1;
+                if($list->menu_item_parent == $objectID_stack[$stackTop])
+                {
+                    if($level == 1)
+                    {
+                        $output = $output."<li class=\"dropdown\"><a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">".$last_title."<b class=\"caret\"></b></a><ul class=\"dropdown-menu\">";
+                        $last_title = $list->title;
+                        $last_url = $list->url;
+                        $level = 2;
+                        array_push($objectID_stack, $list->object_id);
+                    }
+                    else if ($level == 2)
+                    {
+                        $output = $output."<li class=\"dropdown-submenu\"><a href=\"#\">".$last_title."</a><ul class=\"dropdown-menu\">";
+                        $last_title = $list->title;
+                        $last_url = $list->url;
+                        $level = 3;
+                    }
+                    else if($level == 3)
+                    {
+                        $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li>";
+                        $last_title = $list->title;
+                        $last_url = $list->url;
+                        $level = 3;
+                    }       
+                }
+                else
+                {
+                    if($level == 2)
+                    {
+                        array_pop($objectID_stack);
+                        $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li>";
+                        $last_title = $list->title;
+                        $last_url = $list->url;
+                        $level = 2;
+                        array_push($objectID_stack, $list->object_id);
+                    }
+                    if($level == 3)
+                    {
+                        array_pop($objectID_stack);
+                        $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li></ul></li>";
+                        $last_title = $list->title;
+                        $last_url = $list->url;
+                        $level = 2;
+                        array_push($objectID_stack, $list->object_id);
+                    }
+                    
+                }
+            }
         }
-        if($isParent) //If is parent and not printed.
+        if($level == 1) //If is parent and not printed.
             $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li>";
+        else if($level == 2) //If is sub and not printed.
+            $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li></ul></li>";
+        else if($level == 3) //If is sub of sub and not printed.
+            $output = $output."<li><a href=\"".$last_url."\">".$last_title."</a></li></ul></li></ul></li>";
         return $output;
     }
